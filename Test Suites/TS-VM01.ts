78542e00fc3a26ac56321848eab95daf88cc19b4
Premchand.Vishwakarm <Premchand.Vishwakarm@AQMIT-LPT-10781>
2024-05-23 16:35:10 +0530

    First Commit

---
+++ Test Suites/TS-VM01.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-VM01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>610622dc-d59b-47ed-8176-5f0be3627a35</testSuiteGuid>
+   <testCaseLink>
+      <guid>51e07e1a-fc9f-4f20-9f13-5090da41ece3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-VM01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
